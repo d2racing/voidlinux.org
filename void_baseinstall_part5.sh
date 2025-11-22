a11111e131523fbf3e4be6c1fb957cdd4e4c4d5c
Sylvain Alain <d2racing911@gmail.com>
2025-11-21 23:33:02 -0500

    Update void_baseinstall_part5.sh

--- void_baseinstall_part5.sh
+++ void_baseinstall_part5.sh
@@ -9,47 +9,9 @@ echo "start of void_baseinstall_part5"
 
 # Installer les dépendances
 
-xbps-install -Syv alsa-plugins alsa-plugins-32bit alsa-lib alsa-lib-32bit \
-FAudio FAudio-32bit \
-freetype freetype-32bit \
-libXft libXft-32bit \
-flex \
-fluidsynth libfluidsynth libfluidsynth-32bit \
-libXrandr libXrandr-32bit xrandr \
-libldap libldap-32bit \
-mpg123 libmpg123 libmpg123-32bit \
-libXcomposite libXcomposite-32bit \
-libXi libXi-32bit \
-libXinerama libXinerama-32bit \
-libXScrnSaver libXScrnSaver-32bit \
-libopenal libopenal-32bit \
-alsa-pipewire alsa-pipewire-32bit pipewire pipewire-32bit libjack-pipewire libjack-pipewire-32bit \
-mit-krb5 mit-krb5-32bit \
-gnutls gnutls-32bit \
-giflib giflib-32bit \
-gst-libav gst-plugins-ugly1 \
-gst-plugins-bad1 gst-plugins-bad1-32bit \
-gst-plugins-base1 gst-plugins-base1-32bit \
-gst-plugins-good1 gst-plugins-good1-32bit \
-gstreamer1 gstreamer1-32bit \
-libpng libpng-32bit \
-v4l-utils v4l-utils-32bit \
-vulkan-loader vulkan-loader-32bit \
-libgpg-error libgpg-error-32bit \
-libjpeg-turbo libjpeg-turbo-32bit \
-libgcrypt libgcrypt-32bit \
-ncurses ncurses-libs ncurses-libs-32bit \
-ocl-icd ocl-icd-32bit \
-libxslt libxslt-32bit \
-libva libva-32bit \
-glu glu-32bit \
-sqlite sqlite-32bit \
-gtk+3 gtk+3-32bit \
-libpulseaudio libpulseaudio-32bit \
-libnm libnm-32bit \
-gamemode libgamemode libgamemode-32bit \
-vkBasalt vkBasalt-32bit \
-cabextract
+xbps-install -Syv alsa-plugins alsa-lib FAudio freetype libXft flex fluidsynth libfluidsynth libXrandr xrandr libldap mpg123 libmpg123 libXcomposite libXi libXinerama libXScrnSaver libopenal alsa-pipewire pipewire libjack-pipewire  \
+mit-krb5 gnutls giflib gst-libav gst-plugins-ugly1 gst-plugins-bad1 gst-plugins-base1 gst-plugins-good1 gstreamer1 libpng v4l-utils vulkan-loader libgpg-error libjpeg-turbo libgcrypt ncurses ncurses-libs ocl-icd libxslt libva glu sqlite  \
+gtk+3 libpulseaudio libnm gamemode libgamemode vkBasalt cabextract
 
 # Installer Flatpak (Optionnel)
 
