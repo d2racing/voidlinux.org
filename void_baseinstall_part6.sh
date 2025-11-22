1e778a859fa16ffdb0b05df3ac2c98256207aed3
Sylvain Alain <d2racing911@gmail.com>
2025-11-21 23:47:04 -0500

    Update void_baseinstall_part6.sh

--- void_baseinstall_part6.sh
+++ void_baseinstall_part6.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "start of void_baseinstall_part6"
 
-echo 'repository=https://github.com/void-land/hyprland-void-packages-testing/releases/latest/download/' | sudo tee /etc/xbps.d/hyprland-packages.conf
+# echo 'repository=https://github.com/void-land/hyprland-void-packages-testing/releases/latest/download/' | sudo tee /etc/xbps.d/hyprland-packages.conf
+echo 'repository=https://github.com/void-land/hyprland-void-packages/' | sudo tee /etc/xbps.d/hyprland-packages.conf
 xbps-install -S
 xbps-install -Sy hyprland hyprland-devel aquamarine hyprcursor hypridle hyprland-protocols hyprlang hyprlock hyprpaper hyprutils hyprwayland-scanner xdg-desktop-portal-hyprland
 
