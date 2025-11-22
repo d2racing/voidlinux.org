a3128972265ef2d39130787714424c8ee7708948
Sylvain Alain <d2racing911@gmail.com>
2025-11-22 01:00:54 -0500

    Update void_baseinstall_part3.sh

--- void_baseinstall_part3.sh
+++ void_baseinstall_part3.sh
@@ -3,7 +3,7 @@ echo "start of void_baseinstall_part3"
 
 xbps-reconfigure -f glibc-locales
 xbps-install -Syv poppler-data qt5-translations qt6-translations firefox-i18n-fr thunderbird-i18n-fr aspell-fr hunspell-fr_FR libreoffice-i18n-fr manpages-fr 
-xbps-install -Syv gpm htop inxi neofetch git gnome-keyring rtkit libarchive tar xz p7zip unzip zip gzip lz4 lzo zstd linux-firmware linux-firmware-network cpupower spectre-meltdown-checker \
+xbps-install -Syv gpm htop inxi fastfetch git gnome-keyring rtkit libarchive tar xz p7zip unzip zip gzip lz4 lzo zstd linux-firmware linux-firmware-network cpupower spectre-meltdown-checker \
 chrony cronie firefox firefox-i18n-fr polkit polkit-gnome tlp tracker tty-clock Thunar xbps octoxbps dbus dhcpcd xdg-desktop-portal xorg libinput libinput-gestures
 
 echo "end of void_baseinstall_part3"
